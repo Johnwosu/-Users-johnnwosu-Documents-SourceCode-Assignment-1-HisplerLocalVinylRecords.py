46fc70d339dcb6e83d0a7bc8c15beec81d29edd8
Johnwosu <W0524169@nscc.ca>
2025-10-04 23:36:30 -0300

    Add files via upload

---
+++ HisplerLocalVinylRecords.py
@@ -0,0 +1,25 @@
+# Hipster's Local Vinyl Records: Customer's Order Details #
+
+# This program should be able to ask for the customer's name
+# Ask for the delivery distance in Kilometers
+# Ask for the cost of record purchased 
+# Calculate the delivery cost
+# Calculate the purchase cost 
+# Calculate the Total Cost
+# Print purchase summary for customer
+# Delivery Cost:
+# Purchase Cost:
+#Total Cost:
+
+#Important Information#
+deliveryRatePerKM = 15
+salesTax = 0.14
+
+# Ask the customer to input name
+customerName = input("Enter the customer's name: ")
+
+# Delivery distance
+deliveryDistance = float(input("Enter the delivery distance: "))
+
+
+
